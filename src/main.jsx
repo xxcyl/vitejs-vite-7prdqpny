3ad51db0554d6544282a61d2c0a71ac7ea8794f7
xxcyl <wittyxx@gmail.com>
2025-04-03 22:08:54 +0800

    feat: Add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BreathingProvider } from './contexts/BreathingContext.jsx'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
