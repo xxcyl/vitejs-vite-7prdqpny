6d5594820b50b290115ef5653b6615e2ff32de58
xxcyl <wittyxx@gmail.com>
2025-04-04 08:50:38 +0800

    Add Vercel Analytics component to enable website usage tracking

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BreathingProvider>
       <App />
+      <Analytics />
     </BreathingProvider>
   </React.StrictMode>,
 )
